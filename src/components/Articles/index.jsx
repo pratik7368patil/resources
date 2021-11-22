d515cc0858b54056c89ecf184d2ecac487f4755f
Pratik Patil <pratik7368patil@gmail.com>
2021-11-22 23:25:57 +0530

    Added mobile support

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -1,12 +1,28 @@
 import "./index.css";
 import getData from "../../data";
 import buildData from "../../utils";
+import { useState } from "react/cjs/react.development";
+import { useEffect } from "react";
 
 function Articles(props) {
   const data = getData(props.tech.id);
   const renderData = data ? buildData(data) : [];
+  const [showName, setShowName] = useState(false);
 
-  return <div className="article-container">{renderData}</div>;
+  useEffect(function () {
+    if (window.innerWidth <= 800) {
+      setShowName(true);
+    }
+  }, []);
+
+  return (
+    <div className="article-container">
+      {showName ? (
+        <h3 className="res-heading">{props.tech.name} Resources</h3>
+      ) : null}
+      {renderData}
+    </div>
+  );
 }
 
 export default Articles;
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,32 +1,60 @@
 import "./index.css";
 import Articles from "./../Articles";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import getData from "../../data";
 
 function Dashboard() {
   const techList = getData("all");
   const [currentTech, setCurrentTech] = useState(techList[0]);
+  const [showHam, setShowHam] = useState(false);
+
+  useEffect(function () {
+    if (window.innerWidth <= 800) {
+      setShowHam(true);
+    }
+  }, []);
 
   return (
     <div className="dashboard-container">
-      <div className="dashboard-left-container">
-        <h2 className="d-tech-head">Technologies/Topics</h2>
-        {techList.map((tech) => {
-          return (
-            <div
-              className={
-                currentTech.id === tech.id
-                  ? "d-tech-item d-active-tech"
-                  : "d-tech-item"
-              }
-              key={tech.id}
-              onClick={() => setCurrentTech(tech)}
-            >
-              {tech.name}
-            </div>
-          );
-        })}
-      </div>
+      {!showHam ? (
+        <div className="dashboard-left-container">
+          <h2 className="d-tech-head">Technologies/Topics</h2>
+          {techList.map((tech) => {
+            return (
+              <div
+                className={
+                  currentTech.id === tech.id
+                    ? "d-tech-item d-active-tech"
+                    : "d-tech-item"
+                }
+                key={tech.id}
+                onClick={() => {
+                  setCurrentTech(tech);
+                  setShowHam(true);
+                }}
+              >
+                {tech.name}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="ham-icon" onClick={() => setShowHam(false)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </div>
+      )}
       <div className="dashboard-right-container">
         <Articles tech={currentTech} />
       </div>
