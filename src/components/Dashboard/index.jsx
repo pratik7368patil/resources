8349df5c500857afe1e3fcce37534c4903a0d7a9
Pratik Patil <pratik7368patil@gmail.com>
2021-12-05 21:53:37 +0530

    Model Closing feature and other bug fixes

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -32,25 +32,45 @@ function Dashboard() {
     >
       {!showHam ? (
         <div className="dashboard-left-container">
-          <h2 className="d-tech-head">Technologies/Topics</h2>
-          {techList.map((tech) => {
-            return (
-              <div
-                className={
-                  currentTech.id === tech.id
-                    ? "d-tech-item d-active-tech"
-                    : "d-tech-item"
-                }
-                key={tech.id}
-                onClick={() => {
-                  setCurrentTech(tech);
-                  updateHam();
-                }}
+          <div className="d-tech-head-container">
+            <h2 className="d-tech-head">Technologies/Topics</h2>
+            <div className="d-tech-close" onClick={() => updateHam()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-x-square"
+                viewBox="0 0 16 16"
               >
-                {tech.name}
-              </div>
-            );
-          })}
+                <path
+                  style={{ color: "#a8a8a850" }}
+                  d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                />
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+            </div>
+          </div>
+          <div className="d-tech-list">
+            {techList.map((tech) => {
+              return (
+                <div
+                  className={
+                    currentTech.id === tech.id
+                      ? "d-tech-item d-active-tech"
+                      : "d-tech-item"
+                  }
+                  key={tech.id}
+                  onClick={() => {
+                    setCurrentTech(tech);
+                    updateHam();
+                  }}
+                >
+                  {tech.name}
+                </div>
+              );
+            })}
+          </div>
         </div>
       ) : (
         <div className="ham-icon" onClick={() => setShowHam(false)}>
